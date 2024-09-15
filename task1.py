3c8240e6648b9c3369e16ecbd90c42f332c1eab6
Shiron Thalagala <shironceylon@gmail.com>
2024-09-14 23:51:54 -0500

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,53 @@
+'''
+* ISAT 6250-1
+* Assignment: Python Basics & Tools
+* 09/14/2024
+* Shiron Thalagala (W0793591)
+'''
+# Function to calculate median of a given set of numbers
+def findMedian(numArray):
+    sum = 0
+    for n in numArray:
+        sum = sum + n
+    median = sum/len(numArray)
 
+    return(median)
+
+
+# Function to find if the input number is prime or not
+def checkPrime(num):
+    isPrime = False
+    if num > 1:
+        for n in range(2, num):
+            if num % n == 0:
+                isPrime = False
+                break                
+            else:
+                isPrime = True
+                
+    else:
+        isPrime = False
+    
+    return isPrime
+
+
+# Get four numbers from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+num4 = int(input("Enter the fourth number: "))
+
+numArray = [num1, num2, num3, num4]
+
+# Compute the median and report it
+median = findMedian(numArray)
+print(f"\nThe median is {median}\n")
+
+
+# Check for prime numbers using the checkPrime function
+
+for number in numArray:
+    if checkPrime(number):
+        print(f"{number} is a prime number")
+    else:
+        print(f"{number} is not a prime number")
